e271332f7c8d48ac203a6332107100f726a98070
Natalie336 D <natalie.dykstra@outlook.com>
2023-03-16 18:50:22 +0000

    added links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<a href="/about"> go to my about page</a>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1>This is my about me page!</h1>
+
+<a href="/"> go to my home page</a>
