ad7f3caeea60d5e5f02e260da8af5928764b95e6
Natalie336 D <natalie.dykstra@outlook.com>
2023-03-29 13:35:51 +0000

    wednesday

--- src/components/answer.svelte
+++ src/components/answer.svelte
@@ -1,5 +1,29 @@
 <script>
-    let Anwser=true
+    export let Anwser = false;
 </script>
 
-your Answer: {true} 
+{#if answer}
+
+<p>
+    Great job - that was the answer!
+</p>
+
+<p>
+    oh man, that answer wasn't quite right...
+</p>
+{/if}
+
+<script>
+    export let Answer = true;
+</script>
+
+{#if answer}
+
+<p>
+    Great job - that was the answer!
+</p>
+
+<p>
+    oh man, that answer wasn't quite right...
+</p>
+{/if}
