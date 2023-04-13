0f14059c82a4f063c0ee3e1f13bbd631f7deca0e
Natalie336 D <natalie.dykstra@outlook.com>
2023-04-13 19:16:31 +0000

    added styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,47 @@
+<script>
+	import Score from "../components/score.svelte";
+</script>
+
 <div class="background">
-    <div class="layout">
-        <slot />
-    </div>
+	<div class="layout">
+        <div class="header">
+            <Score score={0} />
+        </div>
+        <div class="content">
+		    <slot />
+        </div>
+	</div>
 </div>
 
-<style>
-    .background {
-        padding: 16px;
-        
-        position: absolute;
-        inset: 0;
 
-        background-color:rgba(126, 210, 221, 0.596)
+<style>
+	.background {
+		padding: 32px;
+        display: grid;
+		position: absolute;
+		inset: 0;
+
+		background-color: var(--color-purple);
+	}
+
+	.layout {
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+
+        display: grid;
+        grid-template-rows: auto 1fr;
+        align-items: center;
+	}
+
+    .header,.content {
+        background-color: white;
+        border: 4px solid black;
+        padding: 12px 24px;
     }
 
-    .layout {
-        width: 100%;
-        max-width: 1024px;
-        margin: 0 auto;
-
+    .header {
+        display: flex;
+        justify-content: space-between;
     }
 </style>
-
